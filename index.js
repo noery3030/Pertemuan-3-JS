720cae75dc54f70e8f56c75ebc3a7c461df74d6f
m tsani nur ramdhan <107825724+noery3030@users.noreply.github.com>
2022-11-01 15:43:54 +0700

    JS

---
+++ index.js
@@ -0,0 +1,10 @@
+function bebas(){
+    var x = confirm("yakin gak?🤔")
+    x == false ? "" : angka()
+}
+
+function angka(){
+    var y = document.getElementById("input").value;
+    y == 2 ? alert("wihh😁") : alert("yahh salahh😓")
+
+}
